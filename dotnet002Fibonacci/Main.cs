cc96f1b02928222d7cbf933952e382af7494b0b3
Iaroslav Os <osmnbx@gmail.com>
2022-10-10 16:02:34 +0300

    add/upd

--- dotnet002Fibonacci/Main.cs
+++ dotnet002Fibonacci/Main.cs
@@ -1,10 +1,17 @@
-﻿class Program {
-    public static int fib(int i) {
-        if (i < 2) return 1;
-        return fib(i - 2) + fib(i - 1);
+﻿namespace poop
+{
+    class Program
+    {
+        public static int fib(int i)
+        {
+            if (i <= 2) return 1;
+            return fib(i - 2) + fib(i - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(fib(20));
+        }
     }
 
-    static void Main (string [] args) {
-        System.Console.WriteLine(fib(20));
-    }
 }
