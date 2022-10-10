4e749b7249ff65663a764895bc682c37d379a2d9
Iaroslav Os <osmnbx@gmail.com>
2022-10-10 15:56:58 +0300

    add/upd

--- dotnet002Fibonacci/Main.cs
+++ dotnet002Fibonacci/Main.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program {
+    public static int fib(int i) {
+        if (i < 2) return 1;
+        return fib(i - 2) + fib(i - 1);
+    }
+
+    static void Main (string [] args) {
+        System.Console.WriteLine(fib(20));
+    }
+}
