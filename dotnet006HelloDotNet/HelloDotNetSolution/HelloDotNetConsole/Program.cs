75be665c2d9f749df3f6ad4343dc207a6546e7cc
Iaroslav Os <osmnbx@gmail.com>
2022-10-12 13:12:35 +0300

    add/upd

--- dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/Program.cs
+++ dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/Program.cs
@@ -26,12 +26,26 @@ namespace HelloDotNetConsole
             Console.WriteLine("переменная num1: " + num3 + ".");
 
             float fnum = 249123.2142f;
-            double dnum = 214412552324532423.21231452312312342d;
+            double dnum = 21441255232453242333333333423423433333333333.21231452312312342d;
             Console.WriteLine("переменная fnum: " + fnum);
             Console.WriteLine("переменная dnum: " + dnum);
 
+            string word = "Переменная";
+            Console.WriteLine(word + ": " + dnum);
+            
+            char symbol = 'A';
+            Console.WriteLine("Символ: " + symbol);
+
+            
+            bool isHappy = true;
+            Console.WriteLine("isHappy: " + isHappy);
 
 
+            int num_1 = 0, num_2 = 0;
+            num_1 = Convert.ToInt32(Console.ReadLine());
+            num_2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("num_1: " + num_1 + "\nnum_2: " + num_2);
+            
             // Console.ReadKey(); // считать один символ
         }  
     }
