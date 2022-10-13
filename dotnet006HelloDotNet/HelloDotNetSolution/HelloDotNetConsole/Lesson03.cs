0a09b3cb2c90af3502aebe827293f09d0bf62ab5
Iaroslav Os <osmnbx@gmail.com>
2022-10-13 18:39:35 +0300

    add/upd

--- dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/Lesson03.cs
+++ dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/Lesson03.cs
@@ -2,5 +2,12 @@
 
 public class Lesson03
 {
-    
+    public static void run()
+    {
+        int num4 = 0, num5 = 0;
+        num4 = Convert.ToInt32(Console.ReadLine());
+        num5 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("num4: " + num4 + "\nnum_2: " + num5);
+        // Console.ReadKey(); // считать один символ
+    }
 }
--- dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/Main.cs
+++ dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/Main.cs
@@ -3,6 +3,9 @@
 void Main()
 {
     Console.OutputEncoding = System.Text.Encoding.UTF8; // кирилица в консоли
-    
+    // Lesson01.run();
+    // Lesson02.run();
+    // Lesson03.run();
+    Lesson04.run();
 }
 Main();
