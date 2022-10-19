c29e55ba9269f5ff419ef39ef50d518a0fd518b6
Iaroslav Os <osmnbx@gmail.com>
2022-10-19 16:51:51 +0300

    add/upd

--- dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/Lesson05.cs
+++ dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/Lesson05.cs
@@ -4,6 +4,21 @@ public static class Lesson05
 {
     public static void Run()
     {
-        Console.WriteLine(1);
+        var word = "a";
+        word = "abc";
+        
+        if (word.Length > 2)
+        {
+            Console.WriteLine("cool: ");
+            Console.WriteLine("word.Length > 2");
+        }
+        else
+        {
+            Console.WriteLine("cool: ");
+            Console.WriteLine("word.Length <2");
+        }
+        
+        
+        
     }
 }
