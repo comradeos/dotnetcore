ba3db5fde10cef5905cbe2f216b98aae91a190b1
Iaroslav Os <osmnbx@gmail.com>
2022-10-20 09:38:24 +0300

    add/upd

--- dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/L08Arrays.cs
+++ dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/L08Arrays.cs
@@ -4,18 +4,32 @@ public static class L08Arrays
 {
     public static void Run()
     {
-        byte[] nums = new byte[5];
-        Console.WriteLine(nums[0]);
-        Console.WriteLine(nums[1]);
-        Console.WriteLine(nums[2]);
-        Console.WriteLine(nums[3]);
-        Console.WriteLine(nums[4]);     
+        byte[] nums = new byte[5]; // var nums = new byte[5];
+        nums[0] = 10;
+        nums[1] = 25;
+        nums[2] = 30;
+        nums[3] = 41;
+        nums[4] = 54;
+
+        Console.WriteLine(nums); // System.Byte[]
+        for (var i = 0; i < nums.Length; i++)
+        {
+            Console.WriteLine(nums[i]);
+        }
+
+        string[] words = new string[]
+        {
+            "Ann",
+            "Bob",
+            "Alex",
+        }; // var words = new string[] { "John", "Bob", "Alex", };
+
+        words[1] = "Walley";
         
-        string[] words = new string[5];
-        Console.WriteLine(words[0]);
-        Console.WriteLine(words[1]);
-        Console.WriteLine(words[2]);
-        Console.WriteLine(words[3]);
-        Console.WriteLine(words[4]);
+        for (var i = 0; i < words.Length; i++)
+        {
+            Console.WriteLine(words[i]);
+        }
+        
     }
 }
