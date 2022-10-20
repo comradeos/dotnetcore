fb465423006de99e8cbb55fb9fbf4237c128d1e1
Iaroslav Os <osmnbx@gmail.com>
2022-10-20 16:59:49 +0300

    add/upd

---
+++ dotnet006HelloDotNet/HelloDotNetSolution/NetFrameworkApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetFrameworkApp1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(123);
+        }
+    }
+}
