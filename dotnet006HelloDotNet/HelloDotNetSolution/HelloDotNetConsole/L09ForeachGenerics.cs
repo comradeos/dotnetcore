bd2b95f61020e2fe32c65818319a3ea6072f6142
Iaroslav Os <osmnbx@gmail.com>
2022-10-20 10:27:36 +0300

    add/upd

--- dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/L09ForeachGenerics.cs
+++ dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/L09ForeachGenerics.cs
@@ -1,31 +1,53 @@
 ﻿namespace HelloDotNetConsole;
+using System.Collections.Generic;
+
 using Tools = L00Tools;
 
 public static class L09ForeachGenerics
 {
     public static void Run()
     {
-        short[] nums = {
-            10, 15, 81, 94, 10, 
+        short[] nums =
+        {
+            10, 15, 81, 94, 10,
         }; // массив
 
         foreach (var num in nums)
         {
             Console.WriteLine(num);
-        }       
-        
-        
-        short[,] nums2 = {
-            {1, 2, 3,}, 
-            {4, 5, 6,}, 
-            {7, 8, 9,}, 
+        }
+
+
+        short[,] nums2 =
+        {
+            { 1, 2, 3, },
+            { 4, 5, 6, },
+            { 7, 8, 9, },
         }; // двумерный массив
+
         
         Tools.Line();
+
         
         foreach (var num in nums2)
         {
             Console.WriteLine(num);
         }
+
+
+        var numbers = new List<int>()
+        {
+            341, 23, 551,
+        };  // динамический массив
+        
+        numbers.Add(777);
+        numbers.Add(777);
+        numbers.Add(777);
+
+        foreach (var num in numbers)
+        {
+            Console.WriteLine(num);
+        }
+
     }
 }
