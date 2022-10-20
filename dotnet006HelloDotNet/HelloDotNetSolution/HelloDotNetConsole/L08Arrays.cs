86fe7204deafe3b383a7fe6f16a62687510f5e6c
Iaroslav Os <osmnbx@gmail.com>
2022-10-20 09:32:06 +0300

    add/upd

--- dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/L08Arrays.cs
+++ dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/L08Arrays.cs
@@ -4,6 +4,18 @@ public static class L08Arrays
 {
     public static void Run()
     {
+        byte[] nums = new byte[5];
+        Console.WriteLine(nums[0]);
+        Console.WriteLine(nums[1]);
+        Console.WriteLine(nums[2]);
+        Console.WriteLine(nums[3]);
+        Console.WriteLine(nums[4]);     
         
+        string[] words = new string[5];
+        Console.WriteLine(words[0]);
+        Console.WriteLine(words[1]);
+        Console.WriteLine(words[2]);
+        Console.WriteLine(words[3]);
+        Console.WriteLine(words[4]);
     }
 }
