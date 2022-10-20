6aefbd77bef9abd17ecc72cef5c0f9f5ea45a7da
Iaroslav Os <osmnbx@gmail.com>
2022-10-20 17:31:29 +0300

    add/upd

--- dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/L11FilesStrings.cs
+++ dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/L11FilesStrings.cs
@@ -18,15 +18,7 @@ public static class L11FilesStrings
         
         Console.WriteLine(string.Concat("Hello", "!!!")); // объединение строк
         
-        string connectionString =
-            "Data Source=(local);Initial Catalog=Northwind;"
-            + "Integrated Security=true";
 
-        // Provide the query string with a parameter placeholder.
-        string queryString =
-            "SELECT ProductID, UnitPrice, ProductName from dbo.products "
-            + "WHERE UnitPrice > @pricePoint "
-            + "ORDER BY UnitPrice DESC;";
         
         
     }
