4b7b7519c0a0616a52ec0e30eadf9463b64f8834
Iaroslav Os <osmnbx@gmail.com>
2022-10-25 12:54:54 +0300

    add/upd

--- dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/L12ExceptionsTryCatch.cs
+++ dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/L12ExceptionsTryCatch.cs
@@ -1,9 +1,11 @@
 ﻿namespace HelloDotNetConsole;
+using Tools = L00Tools;
 
 public static class L12ExceptionsTryCatch
 {
     public static void Run()
     {
         Console.WriteLine("L12ExceptionsTryCatch");
+        Tools.Line();
     }
 }
