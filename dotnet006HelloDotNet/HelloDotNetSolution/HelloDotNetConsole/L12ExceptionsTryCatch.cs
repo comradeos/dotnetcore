82c86fe341ef7ee05ce1098665e5cba2d9a5cf23
Iaroslav Os <osmnbx@gmail.com>
2022-10-25 13:17:23 +0300

    add/upd

--- dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/L12ExceptionsTryCatch.cs
+++ dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/L12ExceptionsTryCatch.cs
@@ -8,7 +8,8 @@ public static class L12ExceptionsTryCatch
         
         try
         {
-            var num = Convert.ToInt32(Console.ReadLine());
+            // var num = Convert.ToInt32(Console.ReadLine());
+            var num = Convert.ToInt32("34d1");
             Console.WriteLine("Converted to int32 = " + num);
         }
         catch (FormatException)
@@ -17,9 +18,24 @@ public static class L12ExceptionsTryCatch
             //throw; // завершить программу
         }
 
-        Console.WriteLine(1);
-        Console.WriteLine(2);
-        Console.WriteLine(3);
+        Tools.Line();
+
+        var a = 23;
+        var b = 0;
+        var res = 0;
+
+        try
+        {
+            res = a / b;
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Перехвачена ошибка деления на ноль!");            
+        }
+        
+        Console.WriteLine(res);
+
+
     }
     
     
