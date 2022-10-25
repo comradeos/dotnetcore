7b0eeb72627fb828e0674022bcdb1908108476d6
Iaroslav Os <osmnbx@gmail.com>
2022-10-25 16:04:14 +0300

    add/upd

--- dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/L14ClassesObjects.cs
+++ dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/L14ClassesObjects.cs
@@ -3,11 +3,16 @@
 using Tools = L00Tools;
 using RobotBase = L14Classes.RobotBase;
 
-
 public static class L14ClassesObjects {
     public static void Run() {
         Tools.Line();
-        var r1 = new RobotBase();
+
+        var bot1 = new RobotBase();
+        bot1.name = "MR1";
+        bot1.weight = 987;
+        bot1.coordinates = new[] {
+            0, 0, 0,
+        };
         
     }
 }
