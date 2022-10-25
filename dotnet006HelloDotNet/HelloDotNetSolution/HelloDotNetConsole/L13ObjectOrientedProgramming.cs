444ef29be28051198355587f8aba06928e5a69a8
Iaroslav Os <osmnbx@gmail.com>
2022-10-25 13:56:04 +0300

    add/upd

---
+++ dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/L13ObjectOrientedProgramming.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloDotNetConsole; 
+using Tools = L00Tools;
+
+public static class L13ObjectOrientedProgramming {
+    public static void Run() {
+        Tools.Line();
+        Console.WriteLine("L13ObjectOrientedProgramming");
+        
+    }
+}
--- dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/Main.cs
+++ dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/Main.cs
@@ -13,7 +13,8 @@ void Main() {
     // L09ForeachGenerics.Run();
     // L10FunctionsClassMethods.Run();
     // L11FilesStrings.Run();
-    L12ExceptionsTryCatch.Run();
+    // L12ExceptionsTryCatch.Run();
+    L13ObjectOrientedProgramming.Run();
 }
 
 Main();
