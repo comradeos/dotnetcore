2e56a116f66d0c134c28cc1dc5b5c8e060f1f270
Iaroslav Os <osmnbx@gmail.com>
2022-10-25 15:41:49 +0300

    add/upd

--- dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/L14ClassesObjects.cs
+++ dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/L14ClassesObjects.cs
@@ -1,38 +1,14 @@
 ﻿namespace HelloDotNetConsole;
 
 using Tools = L00Tools;
+using RobotBase = L14Classes.RobotBase;
 
-internal class Robot {
-    private string _type;
-    private string _model;
-
-    public Robot(string model, string type) {
-        this._model = model;
-        this._type = type;
-    }
-
-    public void SetType(string newType) {
-        this._type = newType;
-    }
-
-    public void PrintType() {
-        Console.WriteLine(this._type);
-    }
-
-    public void SetModel(string newModel) {
-        this._model = newModel;
-    }
-
-    public void PrintModel() {
-        Console.WriteLine(this._model);
-    }
-}
 
 public static class L14ClassesObjects {
     public static void Run() {
         Tools.Line();
-        var robot1 = new Robot("D97", "Delivery");
-        robot1.PrintType();
-        robot1.PrintModel();
+        var r1 = new RobotBase("a", "b");
+        r1.PrintType();
+        r1.PrintModel();
     }
 }
