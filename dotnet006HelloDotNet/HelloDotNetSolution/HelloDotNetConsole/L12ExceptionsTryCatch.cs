0a3d5c525c9a64f4798ca8512ab0c23fc75b7969
Iaroslav Os <osmnbx@gmail.com>
2022-10-25 13:04:40 +0300

    add/upd

--- dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/L12ExceptionsTryCatch.cs
+++ dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/L12ExceptionsTryCatch.cs
@@ -5,7 +5,25 @@ public static class L12ExceptionsTryCatch
 {
     public static void Run()
     {
-        var num = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Converted to int32 = " + num);
+        
+        
+        
+        
+        try
+        {
+            var num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Converted to int32 = " + num);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Был введен не тот формат!");
+            throw;
+        }
+
+        Console.WriteLine(1);
+        Console.WriteLine(2);
+        Console.WriteLine(3);
     }
+    
+    
 }
