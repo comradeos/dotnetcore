cdf15c742d7b1baeab9bd25fd87f75f4d80dba6f
Iaroslav Os <osmnbx@gmail.com>
2022-10-26 09:41:12 +0300

    add/upd

--- dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/L15ConstructStatic.cs
+++ dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/L15ConstructStatic.cs
@@ -4,13 +4,20 @@ public class Robot {
     private string _name;
     private int _weight;
     private int[] _coordinates;
-
+    
+    /**
+     * Конструктор для нового объекта.
+     */
     public Robot(string name, int weight, int[] coordinates) {
+        Console.WriteLine("Робот " + _name + " создан!");
         _name = name;
         _weight = weight;
         _coordinates = coordinates;
     }
-
+    
+    /**
+     * Установка новых значений для имени веса и координат.
+     */
     public void SetValues(string newName, int newWeight, int[] newCoordinates) {
         _name = newName;
         _weight = newWeight;
