a5700da46b07231eabc244a13b6367d21440ecf3
Iaroslav Os <osmnbx@gmail.com>
2022-10-26 22:41:16 +0300

    add/upd

--- dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/L18Inheritance.cs
+++ dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/L18Inheritance.cs
@@ -2,6 +2,25 @@
 
 using static L00Tools;
 
+
+public class L18A {
+    private int _numValue;
+
+    public int _Numvalue {
+        get {
+            Console.WriteLine("<<< get _numValue");
+            return 7;
+        }
+        set {
+            Console.WriteLine(">>> set _numValue");
+            this._numValue = value;
+        }
+    }
+    public L18A(int numValue) {
+        this._numValue = numValue;
+    }
+}
+
 public static class L18Inheritance {
     public static void Run() {
       
