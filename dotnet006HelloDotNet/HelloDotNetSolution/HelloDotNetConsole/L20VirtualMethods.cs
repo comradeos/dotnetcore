3e70ba5ceaed4e6ffda003fa67beb9dac79521e0
Iaroslav Os <osmnbx@gmail.com>
2022-10-27 14:00:23 +0300

    add/upd

--- dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/L20VirtualMethods.cs
+++ dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/L20VirtualMethods.cs
@@ -1,8 +1,33 @@
 ﻿namespace HelloDotNetConsole; 
 using static L00Tools;
 
+public class A20 {
+    protected int num;
+
+    public void SetNum(int num) {
+        this.num = num;
+    }
+    
+    public virtual void PrintNum() {
+        Console.WriteLine(this.num);
+    }
+    
+}
+
+public class B20 : A20 {
+    public override void PrintNum() {
+        Console.WriteLine(">>> " + this.num);
+    }
+}
+
 public static class L20VirtualMethods {
     public static void Run() {
-        Line();        
+        var a = new A20();
+        a.SetNum(34);
+        a.PrintNum();
+        
+        var b = new B20();
+        b.SetNum(55);
+        b.PrintNum();
     }
 }
