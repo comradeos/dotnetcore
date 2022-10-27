911eb1b8190a060114c7be0a4f3fcdfe3e1bc10d
Iaroslav Os <osmnbx@gmail.com>
2022-10-27 13:23:52 +0300

    add/upd

--- dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/L19AsIsPointerArray.cs
+++ dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetConsole/L19AsIsPointerArray.cs
@@ -8,8 +8,23 @@ public class C191 {
 
 public static class L19AsIsPointerArray {
     public static void Run() {
-        var c1911 = new C191();
-        c1911.number = 9231;
-        Console.WriteLine(c1911.number);     
+        var c1911 = new C191() {
+            number = 111,
+        };
+        var c1912 = new C191() {
+            number = 222,
+        };
+        var c1913 = new C191() {
+            number = 333,
+        };
+
+        var c191List = new List<C191>();
+        c191List.Add(c1911);
+        c191List.Add(c1912);
+        c191List.Add(c1913);
+
+        Console.WriteLine(c191List[0].number);
+        Console.WriteLine(c191List[1].number);
+        Console.WriteLine(c191List[2].number);
     }    
 }
