95581649046c46fda9500dbff2b34bf2648442f8
Iaroslav Os <osmnbx@gmail.com>
2022-10-28 10:22:00 +0300

    add/upd

---
+++ dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetWeb/Models/Car.cs
@@ -0,0 +1,15 @@
+﻿namespace HelloDotNetWeb.Models;
+
+public class Car {
+    public int Id { set; get; }
+    public string Name { set; get; }
+    public string ShortDescription { set; get; }
+    public string LongDescription { set; get; }
+    public string Image { set; get; }
+    public ushort Price { set; get; }
+    public bool IsFavorite { set; get; }
+    public int Avalible { set; get; }
+    public int CategoryId { set; get; }
+
+    public virtual Category Category { set; get; }
+}
---
+++ dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetWeb/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace HelloDotNetWeb.Models; 
+
+public class Category {
+    public int Id { set; get; }
+    public string CategoryName { set; get; }
+    public int CategoryDescription { set; get; }
+    // public int id { set; get; }
+}
