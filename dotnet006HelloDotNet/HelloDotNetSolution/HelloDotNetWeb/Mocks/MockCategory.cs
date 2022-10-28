8539883aa839a7f8dbe76f2e20538acfa112eb29
Iaroslav Os <osmnbx@gmail.com>
2022-10-28 11:09:51 +0300

    add/upd

--- dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetWeb/Mocks/MockCategory.cs
+++ dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetWeb/Mocks/MockCategory.cs
@@ -1,7 +1,30 @@
 ﻿using HelloDotNetWeb.Interfaces;
+using HelloDotNetWeb.Models;
 
 namespace HelloDotNetWeb.Mocks; 
 
 public class MockCategory : ICarsCategory {
+    
+    public IEnumerable<Category> AllCategories {
+        set {
+            
+        }
+        get {
+            return new List<Category> {
+                new Category {
+                    CategoryName = "Элекстромобили",
+                    CategoryDescription = "Современный вид транспорта"
+                },
+                new Category() {
+                    CategoryName = "Классические автомобили",
+                    CategoryDescription = "Машины с двигателем внутреннего сгорания"
+                }
+            };
+        }
+    }
+    
+    
+    
+    
     
 }
--- dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetWeb/Models/Category.cs
+++ dotnet006HelloDotNet/HelloDotNetSolution/HelloDotNetWeb/Models/Category.cs
@@ -3,6 +3,6 @@
 public class Category {
     public int Id { set; get; }
     public string CategoryName { set; get; }
-    public int CategoryDescription { set; get; }
+    public string CategoryDescription { set; get; }
     public List<Car> Cars { set; get; }
 }
