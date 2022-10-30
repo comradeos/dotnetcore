f21147654b08c23672a8ccc0e2ef0fe9012708ae
Iaroslav Os <osmnbx@gmail.com>
2022-10-30 10:21:33 +0200

    add/upd

--- dotnet006HelloDotNet/BulkyBookSolution/BulkyBookSolution/BulkyBookWeb/Models/Category.cs
+++ dotnet006HelloDotNet/BulkyBookSolution/BulkyBookSolution/BulkyBookWeb/Models/Category.cs
@@ -1,7 +1,10 @@
-﻿namespace BulkyBookWeb.Models; 
+﻿using System.ComponentModel.DataAnnotations;  // для анотаций типа [Key] [Required]
+namespace BulkyBookWeb.Models; 
 
 public class Category {
+    [Key]
     public int Id { set; get; }
+    [Required]
     public string Name { set; get; }
     public int DisplayOrder { set; get; }
     public DateTime CreatedDateTime { set; get; } = DateTime.Now; // значение по умолчанию
