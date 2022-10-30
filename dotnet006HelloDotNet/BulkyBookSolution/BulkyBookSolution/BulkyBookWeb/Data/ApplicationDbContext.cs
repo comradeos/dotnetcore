eceb95e649beaed38bbe0556c608a0e6903f994d
Iaroslav Os <osmnbx@gmail.com>
2022-10-30 11:43:30 +0200

    add/upd

--- dotnet006HelloDotNet/BulkyBookSolution/BulkyBookSolution/BulkyBookWeb/Data/ApplicationDbContext.cs
+++ dotnet006HelloDotNet/BulkyBookSolution/BulkyBookSolution/BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulkyBookWeb.Data;
 
@@ -6,4 +7,5 @@ public class ApplicationDbContext : DbContext {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {
         
     }
+    public DbSet<Category> Categories { set; get; }
 }
