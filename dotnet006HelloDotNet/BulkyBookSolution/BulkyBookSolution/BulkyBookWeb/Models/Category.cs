ecb23697478a140bd53c1bcf446854e728843bd7
Iaroslav Os <osmnbx@gmail.com>
2022-10-30 10:17:31 +0200

    add/upd

---
+++ dotnet006HelloDotNet/BulkyBookSolution/BulkyBookSolution/BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace BulkyBookWeb.Models; 
+
+public class Category {
+    public int Id { set; get; }
+    public string Name { set; get; }
+    public int DisplayOrder { set; get; }
+    public DateTime CreatedDateTime { set; get; } = DateTime.Now; // значение по умолчанию
+    
+}
