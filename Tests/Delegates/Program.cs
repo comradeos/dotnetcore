33a86f245f77f9fa2048b1fbe6729f26975633c5
Iaroslav Os <osmnbx@gmail.com>
2022-11-15 13:37:20 +0200

    added/updated

---
+++ Tests/Delegates/Program.cs
@@ -0,0 +1,28 @@
+﻿class StockExchangeMonitor
+{
+    public delegate void PriceChange(int price);
+    public PriceChange PriceChangeHandler { get; set; } = ShowPrice;
+
+    public void Start()
+    {
+        while (true)
+        {
+            int bankOfAmericaPrice = (new Random().Next(100));
+            PriceChangeHandler(bankOfAmericaPrice);
+            Thread.Sleep(2000);
+        }
+    }
+    public static void ShowPrice(int price)
+    {
+        Console.WriteLine($"New price is ${price}");
+    }
+}
+
+public static class Program
+{
+    public static void Main()
+    {
+        StockExchangeMonitor monitor = new();
+        monitor.Start();
+    }
+}
