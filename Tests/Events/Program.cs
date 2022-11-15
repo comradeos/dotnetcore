cfe1176ef2cb452cd212bc7a6e174193e768fc19
Iaroslav Os <osmnbx@gmail.com>
2022-11-15 16:37:27 +0200

    added/updated

--- Tests/Events/Program.cs
+++ Tests/Events/Program.cs
@@ -1,7 +1,32 @@
-﻿class Program
+﻿class MyEventClass
 {
+    public delegate void MyDelegate(string a, int b); // делегат
+    public static event MyDelegate? MyEvent; // событие
+
+    public static void MyMethod(string myString, int myInt)
+    {
+        Console.WriteLine($">>> {myString} >>> {myInt}");
+    }
+
+    public static void Run()
+    {
+        MyEvent += MyMethod;
+        MyEvent("A", 7);
+    }
+
+}
+
+
+class Program
+{
+    private void PrivateOne(string a, int b)
+    {
+        Console.WriteLine($"---{a}{b}---");
+    }
     public static void Main()
     {
         MyEventClass.Run();
+        MyEventClass.MyDelegate = this.PrivateOne;
+        MyEventClass.MyDelegate("A",9);
     }
 }
