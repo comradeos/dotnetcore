7683ff3104e01f2ec8ece541605e0d1095580e27
Iaroslav Os <osmnbx@gmail.com>
2023-03-03 13:41:14 +0200

    upd

---
+++ Tests/DelegatesEvents/Example2.cs
@@ -0,0 +1,7 @@
+public delegate void Delegate2();
+
+public class Example2
+{
+    public void ShowMessage1() => Console.WriteLine("ShowMessage1");
+    public void ShowMessage2() => Console.WriteLine("ShowMessage2");
+}
---
+++ Tests/DelegatesEvents/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+https://www.youtube.com/watch?v=pP24DbV0uCc
+https://www.youtube.com/watch?v=oDucgj1JDPQ
+*/
+
+public class Program
+{
+    delegate int MyDelegate(int a, int b); /*тип, который хранит ссылку на метод*/
+
+    static int Sum(int a, int b) { return a + b; }
+
+    public static void Main()
+    {
+        MyDelegate myDelegate = new(Sum);
+        Console.WriteLine(myDelegate(1,2));
+
+        Example2 example2 = new();
+        Delegate2 delegate2 = new(example2.ShowMessage1);
+        delegate2 += example2.ShowMessage2;
+        delegate2();
+    }
+
+}
