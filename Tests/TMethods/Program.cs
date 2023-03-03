4a452ce65a31b51bfd506bc223bbeb1e80102ebf
Iaroslav Os <osmnbx@gmail.com>
2023-03-03 14:37:17 +0200

    upd

--- Tests/TMethods/Program.cs
+++ Tests/TMethods/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.OutputEncoding = Encoding.UTF8;
+
+void MyMethod<T>()
+{
+    if (typeof(T) == typeof(string))
+    {
+        Console.WriteLine("Это строка, братан!");
+    } else {
+        Console.WriteLine("Хз шо це но точно не строка, братан!");
+    }
+}
+
 Console.WriteLine("Hello, World!");
+MyMethod<string>();
