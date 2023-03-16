da1b9928832d4421cffa8ee298ea0e5a4cd0bfe4
Iaroslav Os <osmnbx@gmail.com>
2023-03-16 19:38:14 +0200

    added/updated

---
+++ dotnet008Metanit/Metanit/ConsoleTests/Tests/Benchmarks.cs
@@ -0,0 +1,48 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Text;
+
+namespace ConsoleTests.Tests;
+
+internal class Benchmarks
+{
+    public Benchmarks()
+    {
+        BenchmarkRunner.Run<StringTest>();
+    }
+}
+
+public class StringTest
+{
+    string[] numbers =
+    {
+       "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"
+    };
+    [Benchmark]
+    public string WithStringBuilder()
+    {
+        StringBuilder stringBuilder = new();
+
+        foreach (string line in numbers)
+        {
+            stringBuilder.Append(line);
+            stringBuilder.Append(" ");
+        }
+
+        return stringBuilder.ToString();
+    }
+    [Benchmark]
+    public string WithConcatenation()
+    {
+        string result = "";
+        foreach (string line in numbers) result += line + " ";
+        return result;
+    }
+    [Benchmark]
+    public string WithInterpolation()
+    {
+        string result = "";
+        foreach (string line in numbers) result += $"{result}{line} ";
+        return result;
+    }
+}
---
+++ dotnet008Metanit/Metanit/ConsoleTests/Tests/BiraryReadWrite.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace ConsoleTests.Tests;
+
+internal class BiraryReadWrite
+{
+    public BiraryReadWrite()
+    {
+        string str = "hello world";
+
+        byte[] bytesArray = Encoding.ASCII.GetBytes(str);
+
+        foreach (byte item in bytesArray) Console.WriteLine($"{item}");
+
+        Console.WriteLine();
+
+        BinaryWriter binaryWriter = new(File.Open("my_file", FileMode.Create));
+        binaryWriter.Write(bytesArray);
+        binaryWriter.Close();
+
+        BinaryReader binaryReader = new(File.Open("my_file", FileMode.Open));
+        byte[] reader = binaryReader.ReadBytes(200);
+        binaryReader.Close();
+        foreach (byte item in reader) Console.WriteLine($"{item}");
+
+        string s = Encoding.ASCII.GetString(reader);
+        Console.WriteLine(s);
+    }
+}
