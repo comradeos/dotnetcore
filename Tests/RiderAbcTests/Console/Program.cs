46b66e8ed64899670b5e836e74518d86e4977cdd
Iaroslav Os <osmnbx@gmail.com>
2023-05-02 12:12:53 +0200

    updated

---
+++ Tests/RiderAbcTests/Console/Program.cs
@@ -0,0 +1,22 @@
+﻿class Item
+{
+    public string Name { get; set; }
+
+    public Item (string name)
+    {
+        Name = name;
+    }
+}
+
+
+class Program
+{
+    public static void Main()
+    {
+        List<Item> items = new(2);
+        
+        items[0] = new Item("a");
+        
+        Console.WriteLine(items[0].Name );
+    }
+}
