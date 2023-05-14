791e0c5036146547a3ef7de7c4bd0d73d5de98ba
Iaroslav Os <osmnbx@gmail.com>
2023-05-14 16:48:58 +0200

    updated

---
+++ Tests/ABCSolution/ABCSolution/MyLibrary/MyModel.cs
@@ -0,0 +1,18 @@
+﻿namespace MyLibrary;
+
+internal class MyModel
+{
+    public int Id
+    {
+        set
+        {
+            Console.WriteLine($"Setting ID to {value}...");
+        }
+
+        get
+        {
+            return Id;
+        }
+    }
+    public string Name { get; set; } = "";
+}
