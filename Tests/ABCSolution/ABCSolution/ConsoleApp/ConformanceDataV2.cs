bd2d2816ddcbf10361c45f56f7924763d44e843c
Iaroslav Os <osmnbx@gmail.com>
2023-05-25 21:34:15 +0200

    updated

---
+++ Tests/ABCSolution/ABCSolution/ConsoleApp/ConformanceDataV2.cs
@@ -0,0 +1,34 @@
+﻿namespace Celerint;
+
+public class ConformanceDataV2
+{
+    public List<ConformanceTest> ConformanceTestList { get; set; } = new();
+}
+
+public class ConformanceTest
+{
+    public int? ConformanceTestCount { get; set; }
+    public string? TestName { get; set; }
+    public string? TestType { get; set; }
+    public TesterSetup? TesterSetup { get; set; }
+}
+
+public class TesterSetup
+{
+    public int? NumberOfSites { get; set; }
+    public DigitalInstrument? DigitalInstrument { get; set; }
+}
+
+public class DigitalInstrument
+{
+    public string? Name { get; set; }
+    public Driver? Driver { get; set; }
+}
+
+public class Driver
+{
+    public decimal? DeviceInputVoltageHigh { get; set; }
+    public decimal? DeviceInputVoltageClampHigh { get; set; }
+    public decimal? DeviceInputVoltageLow { get; set; }
+    public decimal? DeviceInputVoltageClampLow { get; set; }
+}
--- Tests/ABCSolution/ABCSolution/ConsoleApp/Program.cs
+++ Tests/ABCSolution/ABCSolution/ConsoleApp/Program.cs
@@ -1,9 +1,34 @@
-﻿using MyLibrary;
+﻿using Celerint;
 
 public class Program
 {
+
     public static void Main(String[] args)
     {
+        ConformanceDataV2 data = new();
+
+
+        data.ConformanceTestList.Add(new());
+
+        if (data.ConformanceTestList[^1].TesterSetup != null)
+        {
+            if (data.ConformanceTestList[^1].TesterSetup?.DigitalInstrument != null)
+            {
+                if (data.ConformanceTestList[^1].TesterSetup?.DigitalInstrument?.Driver != null) 
+                {
+                    Console.WriteLine(111111);
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+        /*
         MyModel myModel = new()
         {
             Id = 5532,
@@ -11,5 +36,30 @@ public class Program
         };
 
         Console.WriteLine($"{myModel.Id} :: {myModel.Name}");
+
+        TesterSetup testerSetup = new();
+
+        if (testerSetup.DigitalInstrument != null)
+        {
+            if (testerSetup.DigitalInstrument.Driver != null)
+                testerSetup.DigitalInstrument.Driver.DeviceInputVoltageHigh = 0;
+        }
+
+        */
     }
+}
+
+public class TesterSetup
+{
+    public DigitalInstrument? DigitalInstrument { get; set; }
+}
+
+public class DigitalInstrument
+{
+    public Driver? Driver { get; set; }
+}
+
+public class Driver
+{
+    public decimal? DeviceInputVoltageHigh { get; set; }
 }
