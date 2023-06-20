2b16cd014d5fdd2a89ec0e2af5641529bc5398e1
Iaroslav Os <osmnbx@gmail.com>
2023-06-20 17:17:05 +0200

    updated

---
+++ Tests/ChatGpt/ChatGpt/ChatGptConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ChatGptConsole;
+
+public static class Program
+{
+    public static async Task Main()
+    {
+        var api = new OpenAI_API.OpenAIAPI("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");
+        var result = await api.Completions.GetCompletion("One Two Three One Two");
+        Console.WriteLine(result);
+        // should print something starting with "Three"
+    }
+}
+
--- Tests/RiderAbcTests/Console/Program.cs
+++ Tests/RiderAbcTests/Console/Program.cs
@@ -10,10 +10,39 @@ public class Item
     }
 }
 
+public class ClassA
+{
+    public int Id { get; set; }
+    public List<int> nums { get; set; } = new();
+}
+
 internal static class Program
 {
     public static void Main()
     {
+
+        ClassA i1 = new();
+        i1.nums = new List<int>() { 1, 2, 3 };
+
+        ClassA i2 = new();
+        i1.nums = new List<int>() { 6, 8, 1 };
+
+        List<ClassA> listA = new() { i1, i2 };
+        
+        ClassA? res = listA.
+        
+
+
+
+
+
+
+
+
+
+
+        return;
+        
         List<Item> items = new();
 
         if (items.ElementAtOrDefault(0) == null)
