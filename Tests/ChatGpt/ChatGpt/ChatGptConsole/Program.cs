27363a349000b3625e186724a0466462b273e567
Iaroslav Os <osmnbx@gmail.com>
2023-06-20 17:17:18 +0200

    Update Program.cs

--- Tests/ChatGpt/ChatGpt/ChatGptConsole/Program.cs
+++ Tests/ChatGpt/ChatGpt/ChatGptConsole/Program.cs
@@ -4,7 +4,7 @@ public static class Program
 {
     public static async Task Main()
     {
-        var api = new OpenAI_API.OpenAIAPI("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");
+        var api = new OpenAI_API.OpenAIAPI("key");
         var result = await api.Completions.GetCompletion("One Two Three One Two");
         Console.WriteLine(result);
         // should print something starting with "Three"
