7df33605e9df6b807fc21f03dbdce03e73dc0832
Iaroslav Os <osmnbx@gmail.com>
2023-06-30 07:23:51 +0200

    updated

--- Tests/ServerClient/ServerClient/AppClient/Program.cs
+++ Tests/ServerClient/ServerClient/AppClient/Program.cs
@@ -3,58 +3,37 @@ using System.Text;
 
 HttpListener listener = new();
 
-// установка адресов прослушки
 listener.Prefixes.Add("http://127.0.0.1:8888/");
 
-listener.Start(); // начинаем прослушивать входящие подключения
+listener.Start();
 
 while (true)
 {
-    // получаем контекст
     HttpListenerContext context = await listener.GetContextAsync();
-    HttpListenerRequest request = context.Request;
+    
+    // HttpListenerRequest request = context.Request;
 
-    Console.WriteLine(context.Request.HttpMethod);
-    Console.WriteLine(request.RawUrl);
+    // Console.WriteLine(context.Request.HttpMethod);
+    // Console.WriteLine(request.RawUrl);
+        
+    string inputBody = new StreamReader(context.Request.InputStream).ReadToEnd();
 
-    foreach (string item in request.Headers.Keys)
-    {
-        // Console.WriteLine($"{item}:{request.Headers[item]}");
-    }
-
-    // Get the data from the HTTP stream
-    string body = new StreamReader(context.Request.InputStream).ReadToEnd();
-
-    byte[] bytes = Encoding.UTF8.GetBytes("Data received!");
+    byte[] outputBytes = Encoding.UTF8.GetBytes("Data received!");
+    string outputString = Encoding.UTF8.GetString(outputBytes);
 
     context.Response.StatusCode = 200;
     context.Response.KeepAlive = false;
-    context.Response.ContentLength64 = bytes.Length;
+    context.Response.ContentLength64 = outputBytes.Length;
 
     Stream output = context.Response.OutputStream;
-    output.Write(bytes, 0, bytes.Length);
+
+    output.Write(outputBytes, 0, outputBytes.Length);
     context.Response.Close();
 
-    Console.WriteLine(body);
+    string time = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
 
-    /*
-    var response = context.Response;
-
-    // отправляемый в ответ код htmlвозвращает
-    string responseText = @"ok";
-
-    byte[] buffer = Encoding.UTF8.GetBytes(responseText);
-
-    // получаем поток ответа и пишем в него ответ
-    response.ContentLength64 = buffer.Length;
-    using Stream output = response.OutputStream;
-
-    // отправляем данные
-    await output.WriteAsync(buffer);
-    await output.FlushAsync();
-
-    Console.WriteLine("done");
-    */
+    // Console.WriteLine(inputBody);
+    Console.WriteLine($"[{time}] {outputString}");
 }
 
 // server.Stop();
