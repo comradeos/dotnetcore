706c765f25c193a7fbca66f98025f5fd8ce5bea6
Iaroslav Os <osmnbx@gmail.com>
2023-07-04 21:50:11 +0200

    updated

--- Tests/ServerClient/ServerClient/WebServer/Controllers/PostController.cs
+++ Tests/ServerClient/ServerClient/WebServer/Controllers/PostController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using static WebServer.Helper;
 
 namespace WebServer.Controllers;
@@ -7,6 +9,8 @@ namespace WebServer.Controllers;
 [Route("[controller]")]
 public class PostController : ControllerBase
 {
+    private readonly AppDevices appDevices = new();
+
     private readonly IConfiguration _configuration;
 
     public PostController(IConfiguration configuration)
@@ -22,7 +26,12 @@ public class PostController : ControllerBase
 
         try { amount = Convert.ToDecimal(Request.Form["amount"]);  } catch { Console.WriteLine("Can't convert amount!"); }
 
-        string? address = _configuration.GetValue<string>($"Devices:{id}");
+        // string? address = _configuration.GetValue<string>($"Devices:{id}");
+        
+        Console.WriteLine($"appDevices.Count: {appDevices?.devices?.Count}");
+
+        DevicesListItem? devicesListItem = appDevices?.devices?.Find(i => i.Id == id);
+        string? address = devicesListItem?.Address;
 
         if (amount != 0 && address != null)
         {
@@ -34,5 +43,40 @@ public class PostController : ControllerBase
         }
         
         return Ok("");
+    }
+}
+
+public class DevicesListItem
+{
+    [JsonPropertyName("id")]
+    public string? Id { get; set; }
+    [JsonPropertyName("address")]
+    public string? Address { get; set; }
+}
+
+public class AppDevices
+{
+    private static readonly string SettingsPath = "appdevices.json";
+
+    public List<DevicesListItem>? devices = new();
+
+    public AppDevices()
+    {
+        using (StreamReader streamReader = new(SettingsPath))
+        {
+            devices = JsonSerializer.Deserialize<List<DevicesListItem>>(streamReader.ReadToEnd());
+        }
+
+        if (devices == null)
+        {
+            Console.WriteLine("devices == null");
+            Environment.Exit(0);
+        }
+
+        if (devices?.Count == 0)
+        {
+            Console.WriteLine("devices?.Count == 0");
+            Environment.Exit(0);
+        }
     }
 }
