82f73e2d2e2f156d4e67dbbd306b2a30910b3e94
Iaroslav Os <osmnbx@gmail.com>
2023-08-08 12:26:29 +0300

    updated

---
+++ Tests/TryCatch/Program.cs
@@ -0,0 +1,16 @@
+﻿public static class Program {
+
+    public static int DivByZero(int a, int b) {
+        return a / b;
+    }
+
+    public static void Main()
+    {
+        try {
+            int result = DivByZero(1, 0); 
+            Console.WriteLine(result);
+        } catch (Exception e) {
+            Console.WriteLine($"шось не так: {e}");
+        }
+    }
+}
