412170f727d5eed275e01938b8f6ac06c400ab5b
Iaroslav Os <osmnbx@gmail.com>
2023-12-09 20:56:07 +0200

    updated

--- Tests/WebServicesFrontend/general-api/src/App.js
+++ Tests/WebServicesFrontend/general-api/src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import HelloWorld from './pages/HelloWorld';
 
 
 function App() {
     return (
         <Router>
+            <nav>
+                <Link to="/hello">Hello World</Link>
+                <Link to="/">Main</Link>
+            </nav>
             <Routes>
                 <Route path="/hello" element={<HelloWorld />} />
-                <Route path="/" element={<div>lol</div>} />
+                <Route path="/" element={<h1>Main</h1>} />
             </Routes>
         </Router>
   );
