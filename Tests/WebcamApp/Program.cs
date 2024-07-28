58c0bf31df5af0fae64512e4d9a5f3e15cfac798
Iaroslav Os <osmnbx@gmail.com>
2024-07-28 21:31:56 +0300

    updated

--- Tests/WebcamApp/Program.cs
+++ Tests/WebcamApp/Program.cs
@@ -5,44 +5,46 @@ using System.IO;
 using AForge.Video;
 using AForge.Video.DirectShow;
 
-namespace WebcamCaptureConsole
+namespace WebcamCaptureConsole;
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        // Установка кодировки консоли на UTF-8
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        // Получение списка доступных видеоустройств
+        FilterInfoCollection videoDevices = new(FilterCategory.VideoInputDevice);
+
+        if (videoDevices.Count == 0)
         {
-            // Получение списка доступных видеоустройств
-            var videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
-
-            if (videoDevices.Count == 0)
-            {
-                Console.WriteLine("Видеоустройства не найдены.");
-                return;
-            }
-
-            // Выбор первого доступного устройства
-            var videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
-            videoSource.NewFrame += new NewFrameEventHandler(Video_NewFrame);
-            videoSource.Start();
-
-            Console.WriteLine("Нажмите любую клавишу для завершения...");
-            Console.ReadKey();
-
-            // Остановка захвата видео
-            videoSource.SignalToStop();
-            videoSource.WaitForStop();
+            Console.WriteLine("Видеоустройства не найдены.");
+            return;
         }
 
-        private static void Video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        // Выбор первого доступного устройства
+        VideoCaptureDevice videoSource = new(videoDevices[0].MonikerString);
+        videoSource.NewFrame += new NewFrameEventHandler(Video_NewFrame);
+        videoSource.Start();
+
+        Console.WriteLine("Нажмите любую клавишу для завершения...");
+        Console.ReadKey();
+
+        // Остановка захвата видео
+        videoSource.SignalToStop();
+        videoSource.WaitForStop();
+    }
+
+    private static void Video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+    {
+        // Получение текущего кадра
+        using (Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone())
         {
-            // Получение текущего кадра
-            using (Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone())
-            {
-                // Сохранение кадра в файл
-                string fileName = Path.Combine(Directory.GetCurrentDirectory(), "frame.jpg");
-                bitmap.Save(fileName, ImageFormat.Jpeg);
-                Console.WriteLine($"Кадр сохранен: {fileName}");
-            }
+            // Сохранение кадра в файл
+            string fileName = Path.Combine(Directory.GetCurrentDirectory(), "frame.jpg");
+            bitmap.Save(fileName, ImageFormat.Jpeg);
+            Console.WriteLine($"Кадр сохранен: {fileName}");
         }
     }
 }
